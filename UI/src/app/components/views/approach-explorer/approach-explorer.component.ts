f9b76275b91aac1875a5e9f17209435256b18a86
Tobias Haller <Tobias_Haller@outlook.de>
2022-02-26 13:57:35 +0100

    Fix table sorting

--- UI/src/app/components/views/approach-explorer/approach-explorer.component.ts
+++ UI/src/app/components/views/approach-explorer/approach-explorer.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {RefactoringApproach} from "../../../../../api/repository/models/refactoring-approach";
 import {RefactoringApproachService} from "../../../../../api/repository/services/refactoring-approach.service";
 import {UtilService} from "../../../services/util.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {Router} from "@angular/router";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-approach-explorer',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
   styleUrls: ['./approach-explorer.component.scss']
 })
 export class ApproachExplorerComponent implements OnInit {
+  @ViewChild(MatSort) sort!: MatSort;
 
   displayedColumns: string[] = ['id', 'title', 'year', 'authors', 'actions'];
   dataSource!: MatTableDataSource<RefactoringApproach>;
@@ -19,7 +21,8 @@ export class ApproachExplorerComponent implements OnInit {
 
   constructor(private refactoringApproachService: RefactoringApproachService,
               private utilService: UtilService,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit(): void {
     this.requestRefactoringApproaches();
@@ -29,12 +32,31 @@ export class ApproachExplorerComponent implements OnInit {
     this.refactoringApproachService.listRefactoringApproaches().subscribe({
       next: (response: RefactoringApproach[]) => {
         this.refactoringApproaches = response;
-        this.dataSource = new MatTableDataSource(this.refactoringApproaches)
+        this.setDataSource();
       },
       error: () => {
         this.utilService.callSnackBar('Error: Refactoring approaches could not be retrieved.');
       }
     });
+  }
+
+  setDataSource(): void {
+    this.dataSource = new MatTableDataSource(this.refactoringApproaches);
+    this.dataSource.sortingDataAccessor = (data, sortHeaderId) => {
+      switch (sortHeaderId) {
+        case 'id':
+          return data.refactoringApproachId;
+        case 'title':
+          return data.approachSource?.title;
+        case 'year':
+          return data.approachSource?.year;
+        case 'authors':
+          return data.approachSource?.authors;
+        default: // @ts-ignore
+          return data[sortHeaderId];
+      }
+    };
+    this.dataSource.sort = this.sort;
   }
 
   goToLink(url: string): void {
