ff3e2e2393f175832e59f9a45e7439623135575d
Tobias Haller <Tobias_Haller@outlook.de>
2022-03-05 12:39:06 +0100

    Implement recommendation table with expandable rows

--- UI/src/app/components/views/approach-recommendations/approach-recommendations.component.ts
+++ UI/src/app/components/views/approach-recommendations/approach-recommendations.component.ts
@@ -3,24 +3,35 @@ import { ApproachRecommendationsService } from '../../../services/approach-recom
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { ApproachRecommendation } from '../../../../../api/repository/models/approach-recommendation';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-approach-recommendations',
   templateUrl: './approach-recommendations.component.html',
-  styleUrls: ['./approach-recommendations.component.css']
+  styleUrls: ['./approach-recommendations.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ])
+  ]
 })
 export class ApproachRecommendationsComponent implements OnInit {
   @ViewChild(MatSort) sort!: MatSort;
 
-  displayedColumns: string[] = [
-    'suitabilityScore',
-    'id',
-    'title',
-    'year',
-    'authors',
-    'actions'
-  ];
+  displayedColumns: string[] = ['suitability', 'id', 'title', 'authors'];
   dataSource!: MatTableDataSource<ApproachRecommendation>;
+  expandedElement: ApproachRecommendation | undefined | null;
 
   constructor(private recommendationsService: ApproachRecommendationsService) {}
 
@@ -38,16 +49,14 @@ export class ApproachRecommendationsComponent implements OnInit {
       sortHeaderId: string
     ) => {
       switch (sortHeaderId) {
+        case 'suitability':
+          return data.suitabilityScore;
         case 'id':
           return data.refactoringApproachId;
         case 'title':
           return data.approachSource.title;
-        case 'year':
-          return data.approachSource.year;
         case 'authors':
           return data.approachSource.authors;
-        case 'suitabilityScore':
-          return data.suitabilityScore;
         default:
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
@@ -55,6 +64,39 @@ export class ApproachRecommendationsComponent implements OnInit {
       }
     };
     this.dataSource.sort = this.sort;
+  }
+
+  getColumnData(
+    recommendation: ApproachRecommendation,
+    column: string
+  ): unknown {
+    switch (column) {
+      case 'suitability':
+        return recommendation.suitabilityScore + '%';
+      case 'id':
+        return recommendation.refactoringApproachId;
+      case 'title':
+        return recommendation.approachSource.title;
+      case 'authors':
+        return recommendation.approachSource.authors;
+      default:
+        return recommendation;
+    }
+  }
+
+  getColumnHeader(column: string) {
+    switch (column) {
+      case 'suitability':
+        return 'Suitability';
+      case 'id':
+        return 'ID';
+      case 'title':
+        return 'Title';
+      case 'authors':
+        return 'Authors';
+      default:
+        return 'Unknown';
+    }
   }
 
   goToEdit(recommendation: ApproachRecommendation) {
@@ -68,5 +110,19 @@ export class ApproachRecommendationsComponent implements OnInit {
 
   openRecommendationView(recommendation: ApproachRecommendation) {
     console.log('Maybe remove?', recommendation.refactoringApproachId);
+  }
+
+  getSuitabilityColor(recommendation: ApproachRecommendation, column: string) {
+    if (column !== 'suitability') {
+      return '';
+    }
+
+    if (recommendation.suitabilityScore < 50) {
+      return 'suitability-low';
+    } else if (recommendation.suitabilityScore < 75) {
+      return 'suitability-medium';
+    } else {
+      return 'suitability-high';
+    }
   }
 }
