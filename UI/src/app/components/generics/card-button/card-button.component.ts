60b689036f8a1d0831bf1f7b1272efa86be0f0fb
Tobias Haller <Tobias_Haller@outlook.de>
2022-03-22 11:42:15 +0100

    Adjust card button styling to match framework style

--- UI/src/app/components/generics/card-button/card-button.component.ts
+++ UI/src/app/components/generics/card-button/card-button.component.ts
@@ -13,28 +13,55 @@ export class CardButtonComponent implements OnInit {
   @Input() title = '';
 
   @Input() icon = '';
-  @Input() color: 'primary' | 'accent' | 'warn' | 'brown' | string = 'accent';
+  @Input() color:
+    | 'primary'
+    | 'accent'
+    | 'warn'
+    | 'brown'
+    | 'phase-1'
+    | 'phase-2'
+    | 'phase-3a'
+    | 'phase-3b'
+    | string = 'accent';
 
   backgroundColor = '';
+  borderColor = '';
   textColor = '';
-  acceptedColors: string[] = ['primary', 'accent', 'warn', 'brown'];
+  backgroundColorBrightness = 0;
+  acceptedThemeColors: string[] = ['primary', 'accent', 'warn', 'brown'];
+  acceptedPresetColors: string[] = [
+    'phase-1',
+    'phase-2',
+    'phase-3a',
+    'phase-3b'
+  ];
 
   ngOnInit(): void {
+    this.setColors();
+  }
+
+  setColors(): void {
     if (
-      this.acceptedColors.some(
-        (acceptedColor: string) => acceptedColor === this.color
-      )
+      this.acceptedThemeColors.some((color: string) => color === this.color)
+    ) {
+      this.backgroundColor = `var(--color-${this.color}-lighter)`;
+      this.borderColor = `var(--color-${this.color})`;
+    } else if (
+      this.acceptedPresetColors.some((color: string) => color === this.color)
     ) {
       this.backgroundColor = `var(--color-${this.color})`;
+      this.borderColor = `var(--color-${this.color}-border)`;
     } else {
       this.backgroundColor = this.color;
+      this.borderColor = this.color;
     }
 
     this.evaluateTextColor();
-    console.log(this.textColor);
   }
 
-  // Changed the color of the button text based on the background color contrast to white or black
+  /**
+   * Changes the color of the button text based on the background color brightness to white or black
+   */
   evaluateTextColor(): void {
     const temp_elem = document.body.appendChild(
       document.createElement('backgroundcolorholder')
@@ -54,7 +81,7 @@ export class CardButtonComponent implements OnInit {
       return;
     }
 
-    const brightness = evaluateBrightnessBasedOnRGBA(rgba);
-    this.textColor = brightness > 125 ? 'black' : 'white';
+    this.backgroundColorBrightness = evaluateBrightnessBasedOnRGBA(rgba);
+    this.textColor = this.backgroundColorBrightness > 125 ? 'black' : 'white';
   }
 }
