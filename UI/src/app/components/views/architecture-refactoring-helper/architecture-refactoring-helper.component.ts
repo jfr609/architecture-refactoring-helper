399aec105bf0e37cabde6c34aeeaf4a5fe05046f
₲įṾ€₦¢Ħ¥ Đů₭£☄ <st155857@stud.uni-stuttgart.de>
2023-08-03 23:10:05 +0200

    #48

--- UI/src/app/components/views/architecture-refactoring-helper/architecture-refactoring-helper.component.ts
+++ UI/src/app/components/views/architecture-refactoring-helper/architecture-refactoring-helper.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, Inject,   } from '@angular/core';
 import { APP_TITLE } from '../../../app.constants';
 import { UtilService } from '../../../services/util.service';
 import { ApplicationSettingsDialogComponent } from '../../dialogs/application-settings-dialog/application-settings-dialog.component';
@@ -9,6 +9,12 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { AttributeOptionsService } from 'src/app/services/attribute-options.service';
 import { QualityCategory, RecommendationSuitability } from 'api/repository/models';
 import { ApproachRecommendationService } from 'src/app/services/approach-recommendation.service';
+import { ProjectSessionService } from '../../../../../api/repository/services/project-session-service';
+import { lastValueFrom } from 'rxjs';
+import { RefactoringApproach } from '../../../../../api/repository/models/refactoring-approach';
+import { ProjectSession } from '../../../../../api/repository/models/project-session';
+
+
 
 @Component({
   selector: 'app-architecture-refactoring-helper',
@@ -18,16 +24,19 @@ import { ApproachRecommendationService } from 'src/app/services/approach-recomme
 export class ArchitectureRefactoringHelperComponent {
   applicationTitle = APP_TITLE;
   @ViewChild('sideNav') public sidenav!: MatSidenav;
+  @ViewChild('importInput') importInput!: ElementRef;
 
   readonly QualityCategories = QualityCategory;
   readonly RecommendationSuitability = RecommendationSuitability;
+  //projectSessionService: any;
 
   constructor(
     private permissionService: PermissionService,
     public utilService: UtilService,
     public projectService: ProjectService,
     public attributeOptionsService: AttributeOptionsService,
-    public recommendationService: ApproachRecommendationService
+    public recommendationService: ApproachRecommendationService,
+    public projectSessionService: ProjectSessionService
   ) {}
 
   openSettingsDialog() {
@@ -53,24 +62,57 @@ export class ArchitectureRefactoringHelperComponent {
   changeSession(){
 
   }
-  /*exportDB() {
+  exportDB() {
     lastValueFrom(
-      this.projectService.listProjects({
+      this.projectSessionService.listProjectSessions({
         withDetails: true
       })
-    ).then((refactoringApproaches: RefactoringApproach[]) => {
+    ).then((projectSessions: ProjectSession[]) => {
       const downloadLink: HTMLAnchorElement = document.createElement('a');
-      downloadLink.download = 'approaches.json';
-      const fileContent: string = JSON.stringify(refactoringApproaches);
+      downloadLink.download = 'projectSession.json';
+      const fileContent: string = JSON.stringify(projectSessions);
       downloadLink.href = 'data:text/plain;charset=utf-16,' + fileContent;
       downloadLink.click();
       downloadLink.remove();
     });
-  }*/
-
-  exportDB(){
+  
   }
 
-  importDB(){
+  importDB() {
+    this.importInput?.nativeElement.click();
+  }
+  handleSessionImport(event: Event){
+    const files: FileList | null = (event.currentTarget as HTMLInputElement)
+      .files;
+    if (files != null && files.length > 0) {
+      files[0].text().then((value: string) => {
+        const projectSessions: ProjectSession[] = JSON.parse(value);
+        const promises: Promise<ProjectSession>[] = [];
+        for (const projectSession of projectSessions) {
+          promises.push(
+            lastValueFrom(
+              this.projectSessionService.addProjectSession({
+                body: projectSession
+              })
+            )
+          );
+        }
+  
+        Promise.all(promises)
+          .then(() => {
+            this.utilService.callSnackBar(
+              'Project was imported successfully.'
+            );
+          })
+          .catch((reason) => {
+            console.log(reason);
+            this.utilService.callSnackBar(
+              'Error! Some Project could not be imported. ' +
+                'This can happen if Project already exist or are invalid. ' +
+                'Please check if your selected file is valid.'
+            );
+          });
+      });
+    }
   }
 }
