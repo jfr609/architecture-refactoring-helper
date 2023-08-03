d16bbc86f1c8300a1a37ceaf1d3632a358ecd514
₲įṾ€₦¢Ħ¥ Đů₭£☄ <st155857@stud.uni-stuttgart.de>
2023-08-03 23:07:50 +0200

    #46

--- UI/src/app/components/views/phase-one-views/summary/summary.component.ts
+++ UI/src/app/components/views/phase-one-views/summary/summary.component.ts
@@ -24,12 +24,14 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
 import { StrategicGoals } from 'api/repository/models';
+
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
+  
   isDataLoading = true;
   ratingLevel = RatingLevel;
   enumKeys: any;
@@ -38,6 +40,37 @@ export class SummaryComponent implements OnInit {
   patterns = Patterns;
   strategicGoalsList: any = [];
   selectedStrategicGoals?: StrategicGoals;
+
+  cumulatedimplementedPattern1 = 0;
+  cumulatedimplementedPattern2 = 0;
+  cumulatedimplementedPattern3 = 0;
+  cumulatedpreferredPattern1 = 0;
+  cumulatedpreferredPattern2 = 0;
+  cumulatedpreferredPattern3 = 0;
+  /*chart: any;
+
+  chartOptions = {
+    title:{
+      text: "Total Impressions by Platforms"
+    },
+    animationEnabled: true,
+    axisY: {
+      includeZero: true,
+      suffix: "K"
+    },
+    data: [{
+      type: "bar",
+      indexLabel: "{y}",
+      yValueFormatString: "#,###K",
+      dataPoints: [
+        { label: "Snapchat", y: 15 },
+        { label: "Instagram", y: 20 },
+        { label: "YouTube", y: 24 },
+        { label: "Twitter", y: 29 },
+        { label: "Facebook", y: 73 }
+      ]
+    }]
+  }*/
 
   loadedonce = false;
 
@@ -57,9 +90,11 @@ export class SummaryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
     this.isDataLoading = true;
     Promise.all([
       this.projectService.requestStrategicGoalsAttributes(),
+      this.recommendation()
       //this.attributesService.requeststrategicGoalsAttributes()
     ]).then(() => {
       this.strategicGoalsList = this.projectService.strategicGoals.value;
@@ -76,6 +111,27 @@ export class SummaryComponent implements OnInit {
       this.isDataLoading = false;
     });
   
+  }
+
+  recommendation(){
+    //if(this.cumulatedimplementedPattern1>this.cumulatedpreferredPattern2){
+
+    if(1==1){  
+      //ampel rot
+      const redLight = document.getElementById('red') as HTMLInputElement | null;
+      if(redLight!=null){
+      redLight.checked = true;
+      window.alert("Ampel rot");
+    }
+    }
+    else if(2<1){
+      //ampel grün
+      window.alert("Ampel grün");
+    }
+    else{
+      //ampel gelb
+      window.alert("Ampel gelb");
+    }
   }
   
   addEmptyStrategicGoals(): void {
