430e902ce8d9fac6bebc1dedd3b2a42bd8156644
₲įṾ€₦¢Ħ¥ Đů₭£☄ <st155857@stud.uni-stuttgart.de>
2023-10-22 21:25:18 +0200

    #59 fixed export issue

--- UI/src/app/components/views/phase-one-views/assessment/assessment.component.ts
+++ UI/src/app/components/views/phase-one-views/assessment/assessment.component.ts
@@ -27,23 +27,23 @@ import {
   QualitySublevel,
   Scenario
 } from 'api/repository/models';
-
+import { StorageService } from 'src/app/services/storage.service';
 import { ScenarioService } from 'api/repository/services';
-
 import { AssessmentService } from 'api/repository/services/assessment-service';
+import { OnInit } from '@angular/core';
+import { QualityAttributesComponent } from '../quality-attributes/quality-attributes.component';
+import { local } from 'd3-selection';
 
-
-enum CheckBoxType { CHECKED, NONE };
 @Component({
   selector: 'app-assessment',
   templateUrl: './assessment.component.html',
   styleUrls: ['./assessment.component.css']
 })
-export class AssessmentComponent implements AfterViewInit {
+export class AssessmentComponent implements AfterViewInit, OnInit {
   isDataLoading = true;
   ratingLevel = RatingLevel;
   enumKeys: any;
-  enumKeys2: any;
+  patternOptions: any;
   enumKeys3: any;
   enumKeys1: any;
   languages = Languages;
@@ -73,7 +73,8 @@ export class AssessmentComponent implements AfterViewInit {
 
   deletingScenariosList = new Array<Scenario>();
   newScenariosList = new Array<Scenario>();
-  updatingScenariosList = new Array<Scenario>();
+  updatingScenariosList2 = new Array<Scenario>();
+  updatingScenariosList = QualityAttributesComponent.prototype.updatingScenariosList;
 
   cumulatedimplementedPattern1 = 0;
   cumulatedimplementedPattern2 = 0;
@@ -87,24 +88,68 @@ export class AssessmentComponent implements AfterViewInit {
   updatingStrategicGoalsList = new Array<StrategicGoals>();
 
   selectedValues: string[] = [];
-  valueOccurrences2: { [key: string]: number } = {};
-  valueOccurrences: { [value: string]: number } = {};
-
+  savedrlul: boolean = false;
   constructor(
+    public storageService: StorageService,
     public projectService: ProjectService,
     public attributesService: AttributeOptionsService,
     public strategicGoalsService: StrategicGoalsService,
     public utilService: UtilService,
     public scenarioService: ScenarioService,
     public assessmentService: AssessmentService
+    
+
   ) {
     this.enumKeys = Object.keys(this.languages);
      this.enumKeys1 = Object.keys(this.ratingLevel);
-    this.enumKeys2 = Object.values(this.patterns);
+    this.patternOptions = Object.values(this.patterns);
     this.enumKeys3 = Object.values(this.patterns);
   }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
+    if(this.savedrlul){
+    //this.locadScenarioData();
+    //for(const scenario of this.scenarioList){
+      //this.locadScenarioData2(scenario);
+  //}
+  }
+}
+ngAfterViewInit(): void {
+    //this.locadScenarioData();
+    //if(this.savedrlul){
+      for(let scenario of this.scenarioList){
+        //scenario.implementedPattern = [];
+        //scenario.preferredPattern = '';
+        scenario.explanation = 'ok';
+      }
+      this.locadScenarioData();
+    //}
+    //this.retrieveValues();
+    //this.saveValues();
+    const cat = localStorage.getItem("myCat");
+  
+    
+
+    const lul = localStorage.getItem((`implementedPattern_${1}`));
+    //console.log(lul);
+    //this.scenarioList[0].explanation = cat,
+    //console.log(localStorage.getItem(this.localStorageKey));
+    console.log(cat);
+   
+    //console.log(this.scenarioList[0].explanation);
+    //console.log("scenariolist" + this.scenarioList);
+    //console.log(this.scenarioService.getScenario({id:1}));
+
+    //console.log(this.scenarioService.listScenario());
+    //console.log(QualityAttributesComponent.prototype.scenarioList.length);
+    //console.log(this.scenarioService.listScenario());
+    //console.log(this.scenarioService.listScenario());
+    
+    //console.log(this.strategicGoalsService.listStrategicGoals());
+    //this.scenarioList.forEach((scenario: { data: { [key: string]: any; }; id: any; }) => {
+      //scenario.data = this.storageService.getScenarioData();
+    //});
+    //const savedData = this.storageService.getScenarioData();
     this.isDataLoading = true;
     Promise.all([
       this.projectService.requestStrategicGoalsAttributes(),
@@ -116,28 +161,163 @@ export class AssessmentComponent implements AfterViewInit {
       this.strategicGoalsList = this.projectService.strategicGoals.value;
       this.updatingStrategicGoalsList = Object.assign([], this.strategicGoalsList);
       this.qualityList = this.attributesService.getQualitiesByCategory(
-      this.QualityCategories.Attribute
+        this.QualityCategories.Attribute
       );
       this.isDataLoading = false;
     });
   }
 
-  /*addEmptyStrategicGoals(): void {
-    let emptyStrategicGoals: StrategicGoals = {
-      method: '',
-      owner: '',
-      participants: ''
+  saveScenarioData() {
+    for (let scenarioId = 0; scenarioId < this.scenarioList.length; scenarioId++) {
+      localStorage.setItem(`implementedPattern_${scenarioId}`, JSON.stringify(this.scenarioList[scenarioId].implementedPattern));
+      localStorage.setItem(`preferredPattern_${scenarioId}`, this.scenarioList[scenarioId].preferredPattern);
+      localStorage.setItem(`explanation_${scenarioId}`, this.scenarioList[scenarioId].explanation);
 
-    };
-    this.strategicGoalsList.push(emptyStrategicGoals);
-    this.newStrategicGoalsList.push(emptyStrategicGoals);
+      console.log("implemented pattern saved " + localStorage.getItem(`implementedPattern_${scenarioId}`));
+      console.log("preferred pattern saved " + localStorage.getItem(`preferredPattern_${scenarioId}`));
+      console.log("explanation saved " + localStorage.getItem(`explanation_${scenarioId}`));
+
+    }
+    //for()
+    //localStorage.setItem(`implementedPattern_${0}`, JSON.stringify(this.scenarioList[0].implementedPattern));
+    //localStorage.setItem(`preferredPattern_${0}`, this.scenarioList[0].preferredPattern);
+    //localStorage.setItem(`explanation_${0}`, this.scenarioList[0].explanation);
+   /*console.log("implemented pattern saved " + localStorage.getItem(`implementedPattern_${0}`));
+    console.log("preferred pattern saved " + localStorage.getItem(`preferredPattern_${0}`));
+    console.log("explanation saved " + localStorage.getItem(`explanation_${0}`));
+    localStorage.setItem(`implementedPattern_${1}`, JSON.stringify(this.scenarioList[1].implementedPattern));
+    localStorage.setItem(`preferredPattern_${1}`, this.scenarioList[1].preferredPattern);
+    localStorage.setItem(`explanation_${1}`, this.scenarioList[1].explanation);
+*/
+
+    localStorage.setItem("myCat", "Tom");
+   /* const dataToSave = this.scenarioList.reduce((acc: { [x: string]: { implementedPattern: any; preferredPattern: any; explanation: any; }; }, scenario: { name: string | number; implementedPattern: any; preferredPattern: any; explanation: any; }) => {
+      acc[scenario.name] = {
+        implementedPattern: scenario.implementedPattern,
+        preferredPattern: scenario.preferredPattern,
+        explanation: scenario.explanation,
+      };
+      return acc;
+    }, {});
+    this.storageService.saveScenarioData('1',dataToSave);
+    const cat = localStorage.getItem("myCat");
+   
+    //this.locadScenarioData();*/
+    this.savedrlul = true;
+
+  }
+
+
+  locadScenarioData() {
+
+    console.log("implemented pattern loaded " + localStorage.getItem(`implementedPattern_${0}`));
+    console.log("preferred pattern loaded " + localStorage.getItem(`preferredPattern_${0}`));
+    console.log("explanation loaded " + localStorage.getItem(`explanation_${0}`));
+    
+    
+    this.scenarioList = [];
+
+// Push new scenario objects into the array
+//this.scenarioList.push({
+  //implementedPattern: [],
+  //preferredPattern: '',
+  //explanation: '',
+  // Other properties for the scenario
+//});
+
+    
+//const numberOfScenarios = 2; // Set the number of scenarios you have
+for (let scenarioId = 0; scenarioId < this.scenarioList.length; scenarioId++) {
+  let implementedPattern = JSON.parse(localStorage.getItem(`implementedPattern_${scenarioId}`) || '[]');
+  let preferredPattern = localStorage.getItem(`preferredPattern_${scenarioId}`) || '';
+  let explanation = localStorage.getItem(`explanation_${scenarioId}`) || '';
+
+  // Push the loaded data into the scenarioList
+  /*this.scenarioList.push({
+    implementedPattern,
+    preferredPattern,
+    explanation,
+    // Other properties for the scenario
+  });*/
+    
+    
+    /*this.scenarioList[0] = [
+      {
+        implementedPattern: [],
+        preferredPattern: '',
+        explanation: '',
+        // Other properties for the scenario
+      },
+      // Add more objects as needed
+    ];*/
+    console.log("implemented pattern " + this.scenarioList[0].implementedPattern);
+    console.log("preferred pattern " + this.scenarioList[0].preferredPattern);
+    console.log("explanation " + this.scenarioList[0].explanation);
+
+
+    this.scenarioList[0].implementedPattern = JSON.parse(localStorage.getItem(`implementedPattern_${0}`) || '[]');
+    this.scenarioList[0].preferredPattern = localStorage.getItem(`preferredPattern_${0}`) || '';
+    this.scenarioList[0].explanation = localStorage.getItem(`explanation_${0}`) || '';
+
+
+    console.log("implemented pattern after load " + this.scenarioList[0].implementedPattern);
+
+    console.log("implemented pattern loaded " + localStorage.getItem(`implementedPattern_${1}`));
+    console.log("preferred pattern loaded " + localStorage.getItem(`preferredPattern_${1}`));
+    console.log("explanation loaded " + localStorage.getItem(`explanation_${1}`));
+
+  }
+
+    //console.log(this.scenarioList[0].implementedPattern + " " + this.scenarioList[0].preferredPattern + " " + this.scenarioList[0].explanation);
+    //console.log(this.scenarioList[1].implementedPattern + " " + this.scenarioList[1].preferredPattern + " " + this.scenarioList[1].explanation);
+  }
+
+  loadScenarioData2(): any[] {
+    //let savedData = this.storageService.getScenarioData();
+    //if (savedData) {
+      // Iterate through each scenario in scenarioList and load corresponding data
+      return this.scenarioList.map((scenario: { name: string | number; }) => {
+        return {
+          ...scenario,
+          //implementedPattern: savedData[scenario.name]?.implementedPattern,
+          //preferredPattern: savedData[scenario.name]?.preferredPattern,
+          //explanation: savedData[scenario.name]?.explanation,
+          //implementedPattern: savedData[scenario.name]?.implementedPattern,
+          //preferredPattern: savedData[scenario.name]?.preferredPattern,
+          explanation: "yessir",
+        };
+      });
+    //}
+    //return this.scenarioList;
+    //savedData = ['ok'];
+    //console.log(savedData);
+  }
+
+  saveScenarioData2(scenario: { id: string; data: any; }) {
+    if (scenario) {
+      this.storageService.saveScenarioData(scenario.id, scenario.data);
+    }
+  }
+
+  /*loadScenarioData2() {
+    this.scenarioList.forEach((scenario: { data: any; id: string; }) => {
+      if (scenario) {
+        scenario.data = this.storageService.getScenarioData(scenario.id);
+      }
+    });
   }*/
 
   updateValueOccurrencesImplementedPattern() {
+    //0 machen for neuem count
+    this.assessmentService.cumulatedimplementedPattern1 = 0;
+    this.assessmentService.cumulatedimplementedPattern2 = 0;
+    this.assessmentService.cumulatedimplementedPattern3 = 0;
     for (let patternsOfscenariolist of this.scenarioList) {
       if(patternsOfscenariolist.implementedPattern != null){
         if(patternsOfscenariolist.implementedPattern.includes('Monolith')){
           this.assessmentService.increaseCumulatedImplementedPattern1();
+          //this.scenarioList[0].implementedPattern = patternsOfscenariolist.implementedPattern;
+          //localStorage.setItem(`implementedPattern_${0}`, JSON.stringify(this.scenarioList[0].implementedPattern));
         } if(patternsOfscenariolist.implementedPattern.includes('Microservices')){
           this.assessmentService.increaseCumulatedImplementedPattern2();
         } if(patternsOfscenariolist.implementedPattern.includes('Model-View-Controller')){
@@ -152,6 +332,9 @@ export class AssessmentComponent implements AfterViewInit {
       console.log(this.assessmentService.cumulatedimplementedPattern3);
 }
   updateValueOccurrencesPreferredPattern() {
+    this.assessmentService.cumulatedpreferredPattern1 = 0;
+    this.assessmentService.cumulatedpreferredPattern2 = 0;
+    this.assessmentService.cumulatedpreferredPattern3 = 0;
     for (let patternsOfscenariolist of this.scenarioList) {
       if(patternsOfscenariolist.preferredPattern != null){
         if( patternsOfscenariolist.preferredPattern == 'Monolith'){
@@ -221,10 +404,6 @@ export class AssessmentComponent implements AfterViewInit {
       return false;
     }
   }
-
-  //injectScenario(): Boolean{
-   //return(window.alert("Scenario injected"));
-  //}
 
   addEmptyScenario(): void {
     let emptyScenario: Scenario = {
@@ -377,22 +556,22 @@ export class AssessmentComponent implements AfterViewInit {
   }
 
   createAll() {
-    /*if (this.newAssessmentList.length > 0) {
-      this.newAssessmentList.forEach((e) => {
-        this.assessmentService
-          .addAssessment({
+    if (this.newScenariosList.length > 0) {
+      this.newScenariosList.forEach((e) => {
+        this.scenarioService
+          .addScenario({
             body: e
           })
           .subscribe({
             next: (value) => {},
             error: (err) => {
               console.log(err);
-              this.utilService.callSnackBar('Scenario could not be created.');
+              this.utilService.callSnackBar('Assessment could not be created.');
             }
           });
       });
       this.newAssessmentList.splice(0);
-    }*/
+    }
   }
 
   deleteAll() {
@@ -433,10 +612,50 @@ export class AssessmentComponent implements AfterViewInit {
       this.updatingScenariosList.splice(0);
     }
   }
+  saveValues(){
+
+    this.saveScenarioData();
+    for (let patternsOfscenariolist of this.scenarioList) {
+
+     //patternsOfscenariolist.implementedPattern =  this.scenarioList.implementedPattern;
+     //patternsOfscenariolist.preferredPattern =  this.scenarioList.preferredPattern;
+     //patternsOfscenariolist.explanation =  this.scenarioList.explanation;
+
+    localStorage.setItem('implementedPattern', patternsOfscenariolist.implementedPattern);
+    
+     this.scenarioList.implementedPattern = patternsOfscenariolist.implementedPattern;
+     this.scenarioList.preferredPattern = patternsOfscenariolist.preferredPattern;
+     this.scenarioList.explanation = patternsOfscenariolist.explanation;
+
+    localStorage.setItem('implementedPattern', this.scenarioList.implementedPattern);
+
+     console.log(patternsOfscenariolist.implementedPattern + " " + patternsOfscenariolist.preferredPattern + " " + patternsOfscenariolist.explanation);
+     console.log(this.scenarioList.implementedPattern + " " + this.scenarioList.preferredPattern + " " + this.scenarioList.explanation);
+    }
+
+
+      }
+  retrieveValues(){
+   /* this.scenarioList.implementedPattern = localStorage.getItem('implementedPattern');
+    for(let patternsOfscenariolist of this.scenarioList){
+
+      this.scenarioList.implementedPattern = patternsOfscenariolist.implementedPattern;
+      this.scenarioList.preferredPattern = patternsOfscenariolist.preferredPattern;
+      this.scenarioList.explanation = patternsOfscenariolist.explanation;
+      console.log(patternsOfscenariolist.implementedPattern + " " + patternsOfscenariolist.preferredPattern + " " + patternsOfscenariolist.explanation);
+      console.log(this.scenarioList.implementedPattern + " " + this.scenarioList.preferredPattern + " " + this.scenarioList.explanation);
+     //patternsOfscenariolist.implementedPattern =  this.scenarioList.implementedPattern;
+     //patternsOfscenariolist.preferredPattern =  this.scenarioList.preferredPattern;
+     //patternsOfscenariolist.explanation =  this.scenarioList.explanation;
+    }*/
+  }
+
 
   saveChanges() {
     this.updateValueOccurrencesImplementedPattern();
     this.updateValueOccurrencesPreferredPattern();
+    this.saveScenarioData();
+    localStorage.setItem("myCat", "Tom");
     const data: ConfirmDialogData = {
       title: 'Save Changes?',
       message: `Do you really want to save all changes?`,
@@ -444,7 +663,7 @@ export class AssessmentComponent implements AfterViewInit {
       cancelButtonText: 'Cancel'
     };
     
-    
+  
     /*this.scenarioList[4] = this.scenarioList.implented_using;
     this.scenarioList[5] = this.scenarioList.preferred_patterns;
     this.scenarioList[6] = this.scenarioList.explanation;*/
@@ -460,7 +679,7 @@ export class AssessmentComponent implements AfterViewInit {
       .subscribe({
         next: (data: ConfirmDialogData) => {
           if (data == null) return;
-          this.fireAll();
+          //this.fireAll();
         }
       });
   }
